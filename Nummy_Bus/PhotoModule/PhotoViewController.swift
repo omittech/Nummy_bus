67a45b77f9821f65e39d94bf1bbee0ab2ec0b004
l7wang <ralph9989@gmail.com>
2015-09-08 00:05:55 -0400

    final update for image picker

--- Nummy_Bus/PhotoModule/PhotoViewController.swift
+++ Nummy_Bus/PhotoModule/PhotoViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Photos
 import CoreData
 
-class PhotoViewController: UIViewController, UICollectionViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class PhotoViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let moContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
     
@@ -31,15 +31,26 @@ class PhotoViewController: UIViewController, UICollectionViewDelegate, UIImagePi
     // Define the collection view.
     @IBOutlet var photoList: UICollectionView!
     
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+    /*func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        println("Selected one image!")
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
         
         // Save the image user selected into core data.
+        saveImage(image)
+
+    }*/
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        println("Selected one image!")
         saveImage(image)
     }
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         picker.dismissViewControllerAnimated(true, completion: nil)
+        println("Clicked cancel button!")
+
     }
     
     // Save image into core data.
@@ -53,16 +64,18 @@ class PhotoViewController: UIViewController, UICollectionViewDelegate, UIImagePi
         let imgData = UIImageJPEGRepresentation(img, 1)
         
         photo.pImage = imgData
+        println("Image saved!")
         
     }
     
     // Define cells.
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         // If the indexPath is the last cell, it's a button cell.
-        if (indexPath.row == numOfCell) {
+        if (indexPath.row == numOfCell-1) {
             let cell = collectionView.dequeueReusableCellWithReuseIdentifier("addPhotoCell", forIndexPath: indexPath) as! AddPhotoCell
             cell.addPhotoButton.tag = indexPath.row
             cell.addPhotoButton.addTarget(self, action: "getPhotoFromAlbum:", forControlEvents: .TouchUpInside)
+            println("A button cell is created!")
             return cell
         }
             // Else, a photo cell.
@@ -71,6 +84,7 @@ class PhotoViewController: UIViewController, UICollectionViewDelegate, UIImagePi
             let photo = photos[indexPath.row]
             cell.image?.contentMode = .ScaleAspectFit
             cell.image?.image = UIImage(data: photo.pImage)
+            println("A image cell is created!")
             return cell
         }
     }
@@ -85,29 +99,27 @@ class PhotoViewController: UIViewController, UICollectionViewDelegate, UIImagePi
         return true
     }
     
-    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        
-    }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         picker.delegate = self
+        println("abc")
     }
     
-    override func viewWillAppear(animated: Bool) {
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
         // Load images from core data.
         println("Start loading images...")
         var error: NSError?
         let request = NSFetchRequest(entityName: "Photo")
         photos = moContext?.executeFetchRequest(request, error: &error) as! [Photo]
-        println("loading finished")
+        println("Loading finished!")
+        println(photos.count)
         
         // Set variable numOfCell.
         numOfCell = photos.count+1
         
         // Reload the photoList view.
-        self.photoList.reloadData()
+        photoList.reloadData()
     }
     
     override func didReceiveMemoryWarning() {
--- Nummy_Bus/ViewController.swift
+++ Nummy_Bus/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        var storyboard = UIStoryboard(name: "Photo", bundle: nil)
+        var controller = storyboard.instantiateViewControllerWithIdentifier("PhotoList") as! UIViewController
+        
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
